e2a953bbaa9c9d039c9a590dcb206826bd846ff6
Nikita <frickadelka456@gmail.com>
2019-07-15 22:46:55 +0300

    Initial Commit

---
+++ Sci-fi quotes/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Sci-fi quotes
+//
+//  Created by iosdev on 7/15/19.
+//  Copyright © 2019 iosdev. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
